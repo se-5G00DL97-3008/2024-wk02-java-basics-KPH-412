137c0a7185a76fc84c20d82b1229eed82b20fd64
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 20:07:01 +0200

    E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class E06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which multiplication table to show ?");
+        System.out.println("Which multiplication table to show?");
         int num = scanner.nextInt();
 
         //Choose multiplication to show
         for ( int i = 0 ; i <=10; i++ ) {
-            System.out.println(i + "*" + num + "=" + (num*i));
+            System.out.println(i + " * " + num + " = " + (num*i));
         }
         scanner.close();
     }
