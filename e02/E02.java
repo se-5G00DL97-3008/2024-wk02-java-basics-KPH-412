98213de97c775f068a1a4edabd14004a54978ad9
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:35:10 +0200

    e02.java

--- e02/E02.java
+++ e02/E02.java
@@ -1,5 +1,26 @@
-class E02 {
+import java.util.Scanner;
+
+public class e02 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //Create scanner object to read user input
+
+        //Prompt the user for the first time
+        System.out.println("Give a number :");
+        double x = scanner.nextDouble();
+        //Prompt the 2nd time
+        System.out.println("Give another number :");
+        double y = scanner.nextDouble();
+        scanner.close();
+
+        System.out.println(x + "+" + y + "=" + (x+y));
+        System.out.println(x + "*" + y + "=" + (x*y));
+        System.out.println(x + "-" + y + "=" + (x-y));
+        if  (y != 0) {
+            System.out.println(x + "/" + y + "=" + (x/y));
+        } else {
+            System.out.println("Cannot divided to zero");
+        }
 
     }
+    
 }
