0f2a55f05faeebba83f0dd440c32d3ea28a99484
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:41:05 +0200

    E08.java

--- e08/E08.java
+++ e08/E08.java
@@ -1,5 +1,29 @@
-class E08 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class e08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+        System.out.println("Enter a number (0 to quit)");
+        int num = scanner.nextInt();
+        
+        //Check if the number is 0
+        if (num == 0) {
+            break;
+        }
+
+        if (num > 0) {
+            if (num % 2 == 0) {
+            System.out.println("Even number");
+            } else {
+                System.out.println("Odd number");
+            }
+        }
+        else {
+             System.out.println("Enter a positive number");}
+        
+    } 
+    scanner.close();
     }
 }
