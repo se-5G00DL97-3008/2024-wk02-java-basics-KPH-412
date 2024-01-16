557f77cd0971bc2c226e779b548eeb136e2a45f1
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:39:35 +0200

    E06.java

--- e06/E06.java
+++ e06/E06.java
@@ -1,5 +1,15 @@
-class E06 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class e06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which multiplication table to show ?");
+        int num = scanner.nextInt();
+
+        //Choose multiplication to show
+        for ( int i = 0 ; i <=10; i++ ) {
+            System.out.println(i + "*" + num + "=" + (num*i));
+        }
+        scanner.close();
     }
 }
