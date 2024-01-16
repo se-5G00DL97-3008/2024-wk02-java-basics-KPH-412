38ee6e5bdb6211bff3a86dbee8766a5568049a07
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:36:37 +0200

    E03.java

--- e03/E03.java
+++ e03/E03.java
@@ -1,5 +1,16 @@
-class E03 {
-    public static void main(String[] args) {
 
+    import java.util.Scanner;
+
+public class e03 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num =scanner.nextInt();
+        if (num >=0) {
+            System.out.println("Positive number");
+        } else { System.out.println("Negative number");}
+        scanner.close();
     }
 }
+
