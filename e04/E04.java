0db1375ce80b7d00a561a9b6b509415d4009d739
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:37:20 +0200

    E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,18 @@
-class E04 {
+import java.util.Scanner;
+public class e04 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a day :");
+        int day = scanner.nextInt();
+        System.out.println("Enter a month :");
+        int month = scanner.nextInt();
+        
+        if (day == 24 && month == 12) {
+            System.out.println("Merry Christmas");
+        } else {
+            System.out.println("");
+        }
+        scanner.close();
     }
+    
 }
