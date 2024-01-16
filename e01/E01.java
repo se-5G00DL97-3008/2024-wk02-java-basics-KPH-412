f2f48bdc0dee7997329de31ba75dd772bf1952a3
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 12:10:04 +0200

    E01.java

--- e01/E01.java
+++ e01/E01.java
@@ -1,5 +1,23 @@
 class E01 {
     public static void main(String[] args) {
+//Declare the variables
+        int x = 10;                       // Integer
+        double y = 5.5;                   // Floating-point number
+        boolean flag = true;              // Boolean
+        char letter = 'c';                // Character
+        short year = 2022;                // Short integer
+        long population = 7000000000L;    // Long integer
+        byte num = 100;                   // Byte
+        double pi = 3.13457599923384753929348;  // Double
 
+        //Print out the variables
+        System.out.println( x);
+        System.out.println( y);
+        System.out.println(true);
+        System.out.println(letter);
+        System.out.println(year);
+        System.out.println(population);
+        System.out.println(num);
+        System.out.println(pi);
     }
 }
