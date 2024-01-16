ef45fb571574232032771d3794ac52019aa9cf67
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:45:02 +0200

    E04.java

--- e04/E04.java
+++ e04/E04.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class e04 {
+public class E04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a day :");
