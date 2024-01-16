949072e87addc944cf115767da93c16f6ce59d75
KPH-412 <148779303+KPH-412@users.noreply.github.com>
2024-01-16 19:41:31 +0200

    E09.java

--- e09/E09.java
+++ e09/E09.java
@@ -1,5 +1,34 @@
-class E09 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class e09 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double total = 0; 
+        int count = 0;   
+
+        System.out.println("Give a test score (-1 to quit):");
+
+        double score = scanner.nextDouble();
+
+        while (score != -1) { 
+            total += score; 
+            count++; 
+
+            double average = (double) total / count;
+            System.out.println("Average: " + average);
+         
+            System.out.println("Give a test score (-1 to quit):");
+            score = scanner.nextInt();
+        }
+        
+        if (count > 0) {
+            System.out.println("Program terminated. Total number of scores: " + count);
+        } else {
+            System.out.println("");
+        }
+
+        scanner.close();
     }
 }
